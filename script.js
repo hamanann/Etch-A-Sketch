4dd72fc7806f008e4a3ad5053853972f768b78ac
hamanann <a.ahmed_1805@hotmail.com>
2022-10-19 18:59:23 +0200

    Create grid of square divs

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-function addDiv() {
+function addDiv(dimensions) {
     const newDiv = document.createElement('div');
     const cont = document.querySelector('.container');
     cont.appendChild(newDiv);
     newDiv.classList.add('square');
+    newDiv.style.width = dimensions + '%';
+    newDiv.style.height = dimensions + '%';
 }
 
-function createGrid(dimension) {
-    for (let i = 0; i < dimension * dimension; i++) {
-        addDiv();
+function createGrid(size) {
+    const dimensionPercentage = Math.floor(100.0 / size);
+    for (let i = 0; i < size * size; i++) {
+        addDiv(dimensionPercentage);
     }
 }
 

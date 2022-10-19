add0f3c0167dbd13685f55e366b9898015544846
hamanann <a.ahmed_1805@hotmail.com>
2022-10-19 23:58:25 +0200

    Add buttons and try to add grayscale functionality

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const MAX_SIZE = 100;
-const container = document.querySelector('.container');
+const container = document.querySelector('.gridContainer');
+const rainbowColors = ["violet", "indigo", "blue", "green", "yellow", "orange", "red"];
+const grayscaleColors = ["#E5E5E5", "#CCCCCC", "#B2B2B2", "#999999", 
+    "#7F7F7F", "#666666","#4C4C4C", "#333333", "#191919", "#000000"];
+let currentColor = -1;
+let currentMode = "grayscale";
+
 
 function promtGridSize() {
     const size = +prompt(`Enter grid size (max size is ${MAX_SIZE}):`, 16);
@@ -63,17 +69,52 @@ function addHoverEffect() {
     const squares = document.querySelectorAll('.square');
     squares.forEach(square => {
         square.addEventListener('mouseover', (e) => {
-            e.target.classList.add('passed');
+            // e.target.classList.add('passed');
+            // if (currentMode === 'rainbow') {
+                e.target.style.backgroundColor = nextColor();
+            // } else if (currentMode === 'grayscale') {
+            //     e.target.style.backgroundColor = makeDarker();
+            // }
         });
     });
 }
+
+function nextColor() {
+    currentColor = (currentColor + 1) % 7;
+    return rainbowColors[currentColor];
+}
+
+// function makeDarker(square) {
+//     let style = window.getComputedStyle(squa)
+//     let appliedColor = square.style.getPropertyValue('background-color');
+//     let appliedColorIndex  = grayscaleColors.findIndex(appliedColor);
+//     if (appliedColorIndex === -1) {
+//         return grayscaleColors[0];
+//     } else if (appliedColorIndex < 9) {
+//         return grayscaleColors[appliedColorIndex + 1]
+//     } else {
+//         return grayscaleColors[appliedColorIndex];
+//     }
+// }
 
 
 // initial grid upon opening page
 createGrid(16);
 
 // resize button event listener
-const promptButton = document.querySelector('button.prompt');
+const promptButton = document.querySelector('button.resize');
 promptButton.addEventListener('click', () => {
     promtGridSize();
+});
+
+const rainbowButton = document.querySelector('.rainbow');
+rainbowButton.addEventListener('click', (e) => {
+    currentMode = "rainbow";
+    addHoverEffect();
+});
+
+const grayscaleButton = document.querySelector('.grayscale');
+grayscaleButton.addEventListener('click', () => {
+    currentMode = "grayscale";
+    addHoverEffect();
 });

7ca7abbc380ca1cf94d2d3b6832c06f15e2c6c94
hamanann <a.ahmed_1805@hotmail.com>
2022-10-19 20:27:11 +0200

    Refactored code

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-const MAX_SIZE = 60;
+const MAX_SIZE = 100;
+const container = document.querySelector('.container');
 
+// Adds a single div square of specified dimension to the grid container
 function addDiv(dimensions) {
-    const container = document.querySelector('.container');
     const newDiv = document.createElement('div');
     container.appendChild(newDiv);
     newDiv.classList.add('square');
-    newDiv.style.width = dimensions + '%';
-    newDiv.style.height = dimensions + '%';
+    newDiv.style.width = dimensions;
+    newDiv.style.height = dimensions;
 }
 
+// creates square grid of specified size
 function createGrid(size) {
-    const dimensionPercentage = 100.0 / size;
+    if (isNaN(size)) {
+        size = 16;
+    } else if (size > MAX_SIZE){
+        size = MAX_SIZE;
+    }
+
+    const gridSquareDimensions = (100.0 / size) + "%";
     for (let i = 0; i < size * size; i++) {
-        addDiv(dimensionPercentage);
+        addDiv(gridSquareDimensions);
     }
     
+    // add hover-effect event listener to squares
     addHoverEffect();
 }
 
 function addHoverEffect() {
     const squares = document.querySelectorAll('.square');
-        squares.forEach(square => {
+    squares.forEach(square => {
         square.addEventListener('mouseover', (e) => {
             e.target.classList.add('passed');
         });
@@ -32,25 +41,20 @@ function promtGridSize() {
     const size = +prompt(`Enter grid size (max size is ${MAX_SIZE}):`, 16);
 
     removeGrid();
-
-    if (isNaN(size)) {
-        createGrid(16);
-    } else if (size > MAX_SIZE){
-        createGrid(MAX_SIZE);
-    } else {
-        createGrid(size);
-    }
+    createGrid(size);
 }
 
+// removes grid by deleting all children of .container
 function removeGrid() {
-    const container = document.querySelector('.container');
     while (container.firstChild) {
-        container.removeChild(container.firstChild);
+        container.removeChild(container.lastChild);
     }
 }
 
+// initial grid upon opening page
 createGrid(16);
 
+// resize button event listener
 const promptButton = document.querySelector('button.prompt');
 promptButton.addEventListener('click', () => {
     promtGridSize();
